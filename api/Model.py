94fedb5e7ae9a56300834e5222681eab56916f42
Wouter-Weemaes-IT <78794324+Wouter-Weemaes-IT@users.noreply.github.com>
2022-10-01 11:44:21 +0200

    adding reading legend

--- api/Model.py
+++ api/Model.py
@@ -1,6 +1,7 @@
-import fnmatch
-import os
-import cv2
-from keras import models
-from keras import layers
-import numpy as np
+import pandas as pd
+
+
+trainLegend = pd.read_csv('..//datasets/cv-corpus-11.0-2022-09-21/nl/train.tsv', sep='\t')
+testLegend = pd.read_csv('..//datasets/cv-corpus-11.0-2022-09-21/nl/test.tsv', sep='\t')
+
+print(trainLegend)
