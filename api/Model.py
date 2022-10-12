220046ad6e4afc61ba14fad6e95ebe7485147412
ASTESS <anthony.stessels1@gmail.com>
2022-10-12 15:19:57 +0200

    added markdown file for visulization and edited model to work

--- api/Model.py
+++ api/Model.py
@@ -1,22 +1,26 @@
 import deeplake
+import librosa.display
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy.io import wavfile
+
+sample_rate = 16000
 
 ds = deeplake.load("hub://activeloop/timit-train")
 
 AUDIO_FILE = ds.audios[5].numpy()
 
-import sounddevice as sd
-import soundfile as sf
-
 scaled = np.int16(AUDIO_FILE/np.max(np.abs(AUDIO_FILE)) * 32767)
-wavfile.write('sound.wav', 16000, scaled)
+wavfile.write('sound.wav', sample_rate, scaled)
 filename = 'sound.wav'
 
-data, fs = sf.read(filename, dtype='float64')
-sd.play(data, fs)
-status = sd.wait()
-
-plt.plot(AUDIO_FILE)
+wav = scaled.astype(np.float32) / 32768.0
+wav = wav.reshape(-1)
+print(wav)
+plt.figure(figsize=(14, 5))
+librosa.display.waveplot(wav, sr=sample_rate)
+librosa.display.specshow(librosa.amplitude_to_db(np.abs(librosa.stft(wav)), ref=np.max), y_axis='log', x_axis='time')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Power spectrogram')
+plt.tight_layout()
 plt.show()
