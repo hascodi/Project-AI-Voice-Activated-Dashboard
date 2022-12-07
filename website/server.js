05f839e616726d87f4f0a2bb51a67106f829d6dd
ASTESS <anthony.stessels1@gmail.com>
2022-12-07 10:15:06 +0100

    Revert "Merge remote-tracking branch 'origin/main' into main"
    This reverts commit 94fb2bf42becdb579fc909022048b72166e5f575.

---
+++ website/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const multer  = require('multer')
+const port = 3000;
+
+// Setting up the public directory
+app.use(express.static("public"));
+app.use(cors());
+
+app.listen(port, () => console.log(`listening on port ${port}!`));
