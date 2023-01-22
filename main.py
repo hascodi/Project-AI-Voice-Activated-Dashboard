b8e2837dee8cb461679e966ad3609cc48102c0b3
Toon Van Hoye <Toonvanhoye@outlook.com>
2023-01-22 18:32:57 +0100

    main.py split endpoints + dashboards changes

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import tensorflow as tf
 import numpy as np
 from starlette.middleware.cors import CORSMiddleware
 
-digit = False
-
 app = FastAPI()
 
 origins = ["*"]
@@ -19,21 +17,36 @@ app.add_middleware(
 )
 
 
-@app.post("/uploadfile")
-async def create_upload_file(request: Request):
+@app.post("/word")
+async def word(request: Request):
     form_data = await request.form()
     form_data = form_data.get('file')
     contents = await form_data.read()
 
-    if digit:
-        x, _ = tf.audio.decode_wav(contents, desired_channels=1, desired_samples=8000, )
-        label_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        imported = tf.saved_model.load("NoteBooks/saved-Digit-V2")
+    x, _ = tf.audio.decode_wav(contents, desired_channels=1, desired_samples=16000, )
+    label_names = ['down', 'go', 'left', 'no', 'right', 'stop', 'up', 'yes']
+    imported = tf.saved_model.load("NoteBooks/saved-Words")
 
-    else:
-        x, _ = tf.audio.decode_wav(contents, desired_channels=1, desired_samples=16000, )
-        label_names = ['down', 'go', 'left', 'no', 'right', 'stop', 'up', 'yes']
-        imported = tf.saved_model.load("NoteBooks/saved-Words")
+    x = tf.squeeze(x, axis=-1)
+    x = x[tf.newaxis, :]
+
+    # Predict
+    prediction = imported(x)
+    result = np.argmax(prediction[0])
+    print(label_names[result])
+
+    return label_names[result]  # label_names[result]
+
+
+@app.post("/digit")
+async def digit(request: Request):
+    form_data = await request.form()
+    form_data = form_data.get('file')
+    contents = await form_data.read()
+
+    x, _ = tf.audio.decode_wav(contents, desired_channels=1, desired_samples=48000, )
+    label_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    imported = tf.saved_model.load("NoteBooks/saved-Digit-V2")
 
     x = tf.squeeze(x, axis=-1)
     x = x[tf.newaxis, :]
